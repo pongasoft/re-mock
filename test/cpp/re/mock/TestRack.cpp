bdd8a9c53acc5fd1cd0faefb9148913d74abfb16
Yan Pujante <yan@pongasoft.com>
2021-07-15 11:58:28 -0700

    added test for circular dependency

--- test/cpp/re/mock/TestRack.cpp
+++ test/cpp/re/mock/TestRack.cpp
@@ -147,4 +147,98 @@ TEST(Rack, CVWiring) {
   ASSERT_FLOAT_EQ(4.0, pstValue);
 }
 
+// Rack.CircularWiring
+TEST(Rack, CircularWiring)
+{
+  // Setup: we create a circular dependency
+  // MAUSrcCVDst emits audio and receives CV
+  // MAUDstCVSrc receives audio and emits CV
+  // MAUSrcCVDst -> MAUDstCVSrc for audio
+  // MAUDstCVSrc -> MAUSrcCVDst for CV
+  struct MAUSrcCVDst : public MAUSrc, MCVDst
+  {
+    MAUSrcCVDst(int iSampleRate) : MAUSrc(iSampleRate), MCVDst(iSampleRate){}
+    void renderBatch(const TJBox_PropertyDiff iPropertyDiffs[], TJBox_UInt32 iDiffCount)
+    {
+      MAUSrc::renderBatch(iPropertyDiffs, iDiffCount);
+      MCVDst::renderBatch(iPropertyDiffs, iDiffCount);
+    }
+  };
+
+  const Rack::Extension::Configuration MAUSrcCVDstConfig = [](auto &def, auto &rtc, auto &rt) {
+    // MAUSrcCVDst emits audio
+    def.audio_outputs[MAUSrcCVDst::LEFT_SOCKET] = jbox.audio_output();
+    def.audio_outputs[MAUSrcCVDst::RIGHT_SOCKET] = jbox.audio_output();
+
+    // MAUSrcCVDst receives CV
+    def.cv_inputs[MAUSrcCVDst::SOCKET] = jbox.cv_input();
+
+    // use default bindings
+    rtc = RealtimeController::byDefault();
+
+    // rt
+    rt = Realtime::byDefault<MAUSrcCVDst>();
+  };
+
+  struct MAUDstCVSrc : public MAUDst, MCVSrc
+  {
+    MAUDstCVSrc(int iSampleRate) : MAUDst(iSampleRate), MCVSrc(iSampleRate){}
+    void renderBatch(const TJBox_PropertyDiff iPropertyDiffs[], TJBox_UInt32 iDiffCount)
+    {
+      MAUDst::renderBatch(iPropertyDiffs, iDiffCount);
+      MCVSrc::renderBatch(iPropertyDiffs, iDiffCount);
+    }
+  };
+
+  static const Rack::Extension::Configuration MAUDstCVSrcConfig = [](auto &def, auto &rtc, auto &rt) {
+    // MAUDstCVSrc receives audio
+    def.audio_inputs[MAUDstCVSrc::LEFT_SOCKET] = jbox.audio_input();
+    def.audio_inputs[MAUDstCVSrc::RIGHT_SOCKET] = jbox.audio_input();
+
+    // MAUDstCVSrc emits CV
+    def.cv_outputs[MAUDstCVSrc::SOCKET] = jbox.cv_output();
+
+    // use default bindings
+    rtc = RealtimeController::byDefault();
+
+    // rt
+    rt = Realtime::byDefault<MAUDstCVSrc>();
+  };
+
+
+  Rack rack{};
+
+  auto ex1 = rack.newExtension(MAUSrcCVDstConfig);
+  auto ex2 = rack.newExtension(MAUDstCVSrcConfig);
+
+  // we do not connect the right socket on purpose (no need to create the circular dependency)
+  rack.wire(ex1->getAudioOutSocket(MAUSrcCVDst::LEFT_SOCKET), ex2->getAudioInSocket(MAUDstCVSrc::LEFT_SOCKET));
+  rack.wire(ex2->getCVOutSocket(MAUDstCVSrc::SOCKET), ex1->getCVInSocket(MAUSrcCVDst::SOCKET));
+
+  // we make sure that this does not introduce an infinite loop
+  rack.nextFrame();
+
+  auto &ex1Buffer = ex1->getInstance<MAUSrcCVDst>()->fBuffer;
+  auto &ex1Value = ex1->getInstance<MAUSrcCVDst>()->fValue;
+
+  auto &ex2Buffer = ex2->getInstance<MAUDstCVSrc>()->fBuffer;
+  auto &ex2Value = ex2->getInstance<MAUDstCVSrc>()->fValue;
+
+  ex1Buffer.fill(2.0, 0);
+  ex2Value = 3.0;
+
+  rack.nextFrame();
+
+  // due to circular dependency, and the fact that devices are processed in order of id, ex2 out will be processed
+  // first so the CV value will make in this frame while the audio buffer will make it in the next
+  ASSERT_TRUE(ex2Buffer.check(0.0, 0));
+  ASSERT_FLOAT_EQ(3.0, ex1Value);
+
+  rack.nextFrame();
+
+  // now the audio buffer has caught up
+  ASSERT_TRUE(ex2Buffer.check(2.0, 0));
+  ASSERT_FLOAT_EQ(3.0, ex1Value);
+}
+
 }
